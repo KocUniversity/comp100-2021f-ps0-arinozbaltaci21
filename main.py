5c0950a5add4346eb014c0a78195eeff59122808
ARİN ÖZBALTACI <91727473+arinozbaltaci21@users.noreply.github.com>
2021-10-01 10:30:10 +0000

    My Homework

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 x = int(input("enter a number: "))
 y = int(input("enter a number: "))
-
+t=print(int(x**y))
 import math
 print ("Logarithm base 2 of x is : ", end="")
 print (math.log(2,x))
+print(80414)
